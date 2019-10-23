f5ca442007a7d9a3d2528a460129c15c137bc1ab
Spencer Herzberg <spencer.herzberg@gmail.com>
2019-10-23 08:27:40 -0500

    setup bucket policy and use random for bucket name

--- exercise1/main.tf
+++ exercise1/main.tf
@@ -4,14 +4,35 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "bucket_name" {
-  type = string
+variable "bucket_prefix" {
+  type    = string
+  default = "devopsdsm-workshop"
+}
+
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  upper   = false
+  number  = false
+  special = false
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_prefix}-${random_string.bucket_suffix.result}"
   acl    = "public-read"
-//  policy =
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AddPerm",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::${var.bucket_prefix}-${random_string.bucket_suffix.result}/*"
+        }
+    ]
+}
+EOF
 
   website {
     index_document = "index.html"
@@ -31,11 +52,11 @@ EOF
 }
 
 resource "aws_s3_bucket_object" "index" {
-  bucket = "${aws_s3_bucket.static_site.bucket}"
-  key = "index.html"
-  source = "src/index.html"
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "index.html"
+  source       = "src/index.html"
   content_type = "text/html"
-  etag = "${md5(file("src/index.html"))}"
+  etag         = "${md5(file("src/index.html"))}"
 }
 
 output "url" {
